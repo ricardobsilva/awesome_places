e8782cda9bc51bb071dab4d0c79c68a93b67bc6b
Ricardo <ricardo.b.silva13@gmail.com>
2021-02-17 03:52:42 -0300

    Add attribute name in registration strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,14 @@
-class RegistrationsController < DeviseTokenAuth::SessionsController
-  def update
-    current_user.update!(account_update_params)
+class RegistrationsController < DeviseTokenAuth::RegistrationsController
+  skip_before_action :authenticate_user!
 
+  def update
+    @resource.update!(sign_up_params)
     head :no_content
   end
 
   private
 
-  def account_update_params
-    params.permit(:email, :password, :password_confirmation)
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name)
   end
 end
